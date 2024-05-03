cc35e6ebbd1c2b9f234c88c7c8642f321f1d989e
steve.no <jeaniedaddy@gmail.com>
2024-05-03 13:07:56 -0400

    2-28

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: `<app-server></app-server><app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  allowNewServer = false; 
+  constructor(){
+    console.log(this.allowNewServer);
+    setTimeout(()=> {
+      this.allowNewServer = true;
+      console.log(this.allowNewServer);
+    }, 2000)
+  }
 
 }
