377e710aac3d3825bae88d3f4136d8011a55b45f
steve.no <jeaniedaddy@gmail.com>
2024-05-03 13:23:36 -0400

    2-30

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,12 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false; 
+  serverCreationStatus:string = 'No Server was created!';
+
   constructor(){
     console.log(this.allowNewServer);
     setTimeout(()=> {
       this.allowNewServer = true;
       console.log(this.allowNewServer);
     }, 2000)
+  }
+  onClickAddServer(){
+    this.serverCreationStatus = 'Server was created';
   }
 
 }
